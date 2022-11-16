b44452df2b3d62e09d2b3fdc3ccddc90d55462b0
haloandfable <tylerallen@live.com>
2022-11-16 12:27:25 -0500

    update

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import '../styles/SignUp.scss';
+import '../styles/SignUp.scss';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
